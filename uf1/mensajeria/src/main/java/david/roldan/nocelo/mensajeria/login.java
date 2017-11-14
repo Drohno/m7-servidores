269343916c3cb3e17fdbbf2fc001905783643f77
Drohno <david.roldan.nocelo@gmail.com>
2017-11-14 23:04:07 +0100

    Latest

--- uf1/mensajeria/src/main/java/david/roldan/nocelo/mensajeria/login.java
+++ uf1/mensajeria/src/main/java/david/roldan/nocelo/mensajeria/login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.mensajeria;
+package david.roldan.nocelo.mensajeria;
 
 import java.io.*;
 import java.util.Date;
